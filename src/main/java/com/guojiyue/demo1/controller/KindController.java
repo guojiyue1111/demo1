eb600e309c9553fa9145743445058f2a637deebd
guojiyue1111 <59834994+guojiyue1111@users.noreply.github.com>
2020-03-26 22:54:27 +0800

    44
    44

--- src/main/java/com/guojiyue/demo1/controller/KindController.java
+++ src/main/java/com/guojiyue/demo1/controller/KindController.java
@@ -17,7 +17,7 @@ import java.io.PrintWriter;
 @Controller
 @RequestMapping(value = "/kind")
 public class KindController {
-    // 数据访问类99
+    // 数据访问类9988
     @Autowired
     KindMapper kindMapper;
 
